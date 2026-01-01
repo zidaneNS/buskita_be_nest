a27921840d0c99047a6e0f807963108bb451be6a
zidaneNS <zidane.nur004@gmail.com>
2026-01-01 20:54:21 +0700

    set cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000']
+    origin: ['http://localhost:3000', 'https://buskita.vercel.app']
   });
   
   const config = new DocumentBuilder()
